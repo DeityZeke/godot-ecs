358e956bc6f5dfec561b0628e567c12e1dae4191
Nathan <deityzeke@gmail.com>
2025-11-11 20:30:01 -0600

    merged in the new chunk processing pipeline

--- Tests/Benchmarks/EntitySignatureBench/obj/Debug/net8.0/EntitySignatureBench.AssemblyInfo.cs
+++ Tests/Benchmarks/EntitySignatureBench/obj/Debug/net8.0/EntitySignatureBench.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntitySignatureBench")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77fc772a6dee0b4fbb11f2651f66dc29dae6c897")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2077545284ea2f3a066aef31c2c79929087c1c79")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntitySignatureBench")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntitySignatureBench")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
